1f99120e5686a2a11c004d9c610879d12e0aeae1
stimocin <sercan.timocin@nesine.com>
2020-04-24 00:40:42 +0300

    first test added

--- Persistance.Test/ProductRepositoryTest.cs
+++ Persistance.Test/ProductRepositoryTest.cs
@@ -2,16 +2,15 @@
 using Domain.Products;
 using Infrastucture.Persistance;
 using Microsoft.Extensions.Options;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace Persistance.Products
 {
-    [TestClass]
     public class ProductRepositoryTest
     {
         IOptions<Settings> _options;
@@ -21,20 +20,21 @@ namespace Persistance.Products
             _options = Options.Create<Settings>(new Settings());
         }
 
-        [TestMethod]
+        [Fact]
         public async Task Should_GetproductAsync()
         {
             var mockDataHelper = new Mock<IDataHelper>();
-            //mockDataHelper.Setup(x => x.QuerySingleAsync<ProductEntity>(It.IsAny<string>(), It.IsAny<object>()))
-            //    .Returns(Task.FromResult(new ProductEntity() { Id = 1, Name = "MockProduct1", Price = 159.99M }));
+
+            mockDataHelper.Setup(x => x.QuerySingleAsync<ProductEntity>(It.IsAny<string>(), It.IsAny<object>()))
+                .Returns(Task.FromResult(new ProductEntity() { Id = 1, Name = "MockProduct1", Price = 159.99M }));
 
             var productRepo = new ProductRepository(mockDataHelper.Object);
 
             var sut = await productRepo.GetProduct(1);
 
-            Assert.AreEqual(1, sut.Id);
-            Assert.AreEqual("MockProduct1", sut.Name);
-            Assert.AreEqual(159.99M, sut.Price);
+            Assert.Equal(1, sut.Id);
+            Assert.Equal("MockProduct1", sut.Name);
+            Assert.Equal(159.99M, sut.Price);
         }
     }
 }
