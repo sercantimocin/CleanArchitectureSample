8ec2225dbdf945129d696836b9feb0a80eafcf7b
stimocin <sercan.timocin@nesine.com>
2020-04-26 02:00:18 +0300

    cache

---
+++ Infrastucture/Caching/CachingHandler.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Infrastucture.Caching
+{
+    public class CachingHandler<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            //TODO Redis cacher
+            return await next();
+        }
+    }
+}
